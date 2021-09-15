bf50c2320003b8dbfc2a331f76d11c0b397a0c69
+ <+>
2021-09-15 17:33:57 -0500

    xinyi's edit

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
 const BASE_URL_1 = 'https://thronesapi.com/api/v2/Characters'
 const BASE_URL_2 = 'https://got-quotes.herokuapp.com/quotes'
+let houseNames = [];
 
 function getCharacters() {
     fetch(BASE_URL_1)
     .then(resp => resp.json())
-    .then(charArray => charArray.forEach(charObj => renderCharacters(charObj)))
-    console.log(charObj)
+    .then(charArray => charArray.forEach(function(charObj) {
+        houseNames.push(charObj.family) //populate array with house names
+    }))
+    
+}
+getCharacters()
+const houseArray = houseNames;
+console.log(houseArray)
+
+
+
+
+const dropDown1 = document.getElementById('house-list-1')
+dropDown1.addEventListener('change', sortHouses1)
+
+function sortHouses1(e) {
+    let lastName = e.target.value // stark, lannister, targaryen, greyjoy, tyrell
+    let result = houseArray.filter(house => house === lastName)
+    
+        const list = document.createElement('li')
+        list.innerHTML = result
+        document.querySelector('div').append(list)
+
+    
+}
+
+const dropDown2 = document.getElementById('house-list-2')
+dropDown2.addEventListener('change', sortHouses2)
+
+function sortHouses2(e) {
+    let lastName = e.target.value // stark, lannister, targaryen, greyjoy, tyrell
+    console.log(lastName)
+    let result = houseArray.filter(house => house === lastName)
+    console.log(result)
+    
+        const list = document.createElement('li')
+        list.innerHTML = result
+        document.querySelector('div').append(list)
+
     
 }
 
 function getQuotes() {
     fetch(BASE_URL_2)
     .then(resp => resp.json())
-    .then(quotesArray => console.log(quotesArray))
+   // .then(quotesArray => console.log(quotesArray))
 
 }
 getQuotes()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Put "Choose House 1 or 2 next to the drop down menu"
 // Find final picture of Ice Kingdom w/ zombie hoard *done
 // Paragraph about Ice Kingdom *done
 // Background image of Dany (Kevin)
@@ -32,10 +85,10 @@ getQuotes()
 
 
 
-function init() {
-    getCharacters()
-}
-init()
+// function init() {
+//     getCharacters()
+// }
+// init()
 
 
 
